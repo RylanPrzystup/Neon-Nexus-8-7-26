eb718f7fb889cd611ea6add7842332ff43de10c1
RylanPrzystup <rylanprzystup9@gmail.com>
2026-08-25 09:16:02 -0700

    Finished my theme switching, missing media quries

--- shared/theme.js
+++ shared/theme.js
@@ -6,13 +6,33 @@ if(localStorage.getItem("theme") == null){
 
 function checkTheme(){
     if (localStorage.getItem('theme') == "light"){
-        document.querySelector(".promotionsSection").style.backgroundColor = "white";
-        document.querySelector("#promtotionHeader").style.color = "black";
+        
+        if(document.querySelector(".promotionsSection")){
+            document.querySelector(".promotionsSection").style.backgroundColor = "white";
+            document.querySelector("#promtotionHeader").style.color = "black";}
+
+        else if(document.querySelector(".leftSide")){
+            document.querySelector(".rightSide").style.backgroundColor = "white";
+            document.querySelector(".leftSide").style.backgroundColor = "white";
+            if(document.querySelector('main').classList.remove('forced-theme')){
+            document.querySelector('main').classList.remove('forced-theme');}
+            else{}
+        }
         
     }
+
+
     else{
-        document.querySelector(".promotionsSection").style.backgroundColor = "#3a3a49";
-        document.querySelector("#promtotionHeader").style.color = "white";
+
+        if(document.querySelector(".promotionsSection")){
+            document.querySelector(".promotionsSection").style.backgroundColor = "#3a3a49";
+            document.querySelector("#promtotionHeader").style.color = "white";
+        }else if(document.querySelector(".leftSide")){
+            document.querySelector(".rightSide").style.backgroundColor = "#3a3a49";
+            document.querySelector(".leftSide").style.backgroundColor = "#3a3a49";
+            document.querySelector('main').classList.add('forced-theme');
+        }
+
 
     }
 }
